e69d14b1a10032fc6dc35dec774feffd69761412
prachipatilcloud <misspatil.04@gmail.com>
2025-12-17 17:28:22 +0530

    feat: Implement AI image generation and background removal features; integrate user authentication and loading states

--- client/src/pages/RemoveBg.jsx
+++ client/src/pages/RemoveBg.jsx
@@ -1,13 +1,44 @@
 import { Eraser, Sparkles } from 'lucide-react'
 import React, { useState } from 'react'
+import axios from 'axios'
+import toast from 'react-hot-toast';
+import { useAuth } from '@clerk/clerk-react';
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const RemoveBg = () => {
 
   const [input, setInput] = useState('')
-    
-  const onSubmitHandler = async(e) => {
+
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState('')
+
+  const { getToken } = useAuth()
+
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true)
+
+      const formData = new FormData();
+      formData.append('image', input);
+
+      const { data } = await axios.post('/api/ai/remove-image-background', formData, {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`
+        }
+      })
+      if (data.success) {
+        setContent(data.content)
       }
+      else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+    setLoading(false)
+  }
 
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
@@ -19,37 +50,46 @@ const RemoveBg = () => {
         </div>
         <p className='mt-6 text-sm font-medium'>Upload Image</p>
 
-        <input 
+        <input
           onChange={(e) => setInput(e.target.files[0])}
           accept='image/*'
-          type="file" className='w-full text-gray-600 p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' 
-          required/>
+          type="file" className='w-full text-gray-600 p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300'
+          required />
 
         <p className='text-xs text-gray-500 font-light mt-1'>Supports JPG, PNG and other image formats</p>
-        
-        <button className='w-full flex justify-center items-center gap-2 bg-linear-to-r from-[#f6ab41] to-[#ff4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-          <Eraser className='w-5'/> 
+
+        <button 
+        disabled={loading}
+        className='w-full flex justify-center items-center gap-2 bg-linear-to-r from-[#f6ab41] to-[#ff4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+          {
+            loading ? <span className='w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin'></span> : <Eraser className='w-5' />
+          }
           Remove Background
         </button>
-
-
       </form>
 
       {/* right col */}
       <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 '>
         <div className='flex items-center gap-3'>
-          <Eraser className='w-5 h-5 text-[#ff4938]'/>
+          <Eraser className='w-5 h-5 text-[#ff4938]' />
           <h1 className='text-xl font-semibold'>Processed Image</h1>
         </div>
-        <div className='flex flex-1 justify-center items-center'>
+        {
+          !content ? (
+            <div className='flex flex-1 justify-center items-center'>
           <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-            <Eraser className='w-9 h-9'/>
+            <Eraser className='w-9 h-9' />
             <p>Enter a topic and click "Remove Background" to get started</p>
 
           </div>
 
         </div>
-          
+          ) : (
+            <img src={content} alt="image" className='mt-3 w-full h-full'/>
+          )
+        }
+        
+
       </div>
     </div>
   )
